dc5fea17dba16a342c2ca389a838da74cfae4c62
p_917520 <david.carmo@camara.leg.br>
2020-03-02 18:12:55 -0300

    [Update] secund commit

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page ajuda!{{title}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About',
+  data(){
+    return{
+      title: process.env.VUE_APP_TITLE
+    }
+  }
+}
+</script>
+
